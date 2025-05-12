8e88f446f2d6cbb56c00788fda9a8da1dea510a9
mazumdarsaurabh <mazumdarsaurabh@gmail.com>
2025-05-12 14:59:22 +0530

    Correction done on Media

--- inventory_control/settings.py
+++ inventory_control/settings.py
@@ -118,7 +118,7 @@ USE_L10N = True
 USE_TZ = True
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Static files (CSS, JavaScript, Images)
