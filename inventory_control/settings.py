5b52cf2d25fb5bc8b91e38dba15fcf81026f5723
mazumdarsaurabh <mazumdarsaurabh@gmail.com>
2025-05-12 14:42:28 +0530

    Remove /src file

--- inventory_control/settings.py
+++ inventory_control/settings.py
@@ -125,7 +125,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'inventory/static']
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
